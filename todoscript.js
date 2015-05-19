113da81946f3a4d9383230f1f496e76b4684e49b
Lorien Smyer <lorien@Loriens-MacBook-Air.local>
2015-05-19 00:26:25 -0700

    remove comments, code is working

--- todoscript.js
+++ todoscript.js
@@ -1,15 +1,3 @@
-/* What I don't need to do:
-I don't think I need the .completed css; it includes line-through.
-The body is already there.
-I did h1 title in HTML. 
-I made the button in HTML and gave it "add_todo" ID.
-Input box (light grey text box with blue border)
-List items in darker grey
-
-What I need to do:
-My event listeners aren't working, even though
-I know from  */
-
 var ulist = document.querySelector("ul");
 
 var input = document.querySelector("input");
